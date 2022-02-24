83bf939819dfd2e86411260c62431be748e40b76
Harry Rendell <harryrendell9@gmail.com>
2022-02-24 09:08:24 +0000

    updates

--- funcs/plotting.py
+++ funcs/plotting.py
@@ -33,26 +33,26 @@ def plot_classifications(x_test, y_test, predicted):
     plt.subplots_adjust(wspace=1, hspace=1)
 
 
-def plot_training(history):
+def plot_training(history, **kwargs):
     """
     Plot training history with training and test accuracies
 
     Parameters
     ----------
     history : This is the output from model.fit()
-
+    kwargs : optional keyword arguments to pass to Axes.set()
     """
     fig, axes = plt.subplots(1,2, figsize = (16,5))
     for ax, name in zip(axes, ['loss','accuracy']):
         ax.plot(history.history[name])
         ax.plot(history.history['val_'+name])
-        ax.set(title='model '+name, xlabel='epoch', ylabel=name)
+        ax.set(title='model '+name, xlabel='epoch', ylabel=name, **kwargs)
     axes[0].legend(['train', 'test'])
     axes[1].legend(['train', 'test'])
 
 def plot_data(x, y):
     """
-    Plot some of the data to see what we're working with. Label using the true labels.
+    Plot a random subset of the data to see what we're working with. Label using the true labels.
 
     Parameters
     ----------
@@ -62,8 +62,17 @@ def plot_data(x, y):
     """
     if x.shape[-1] == 1:
         x = x.reshape(x.shape[:-1])
+
+    nrows = 8
+    ncols = 8
+    shuffled_idxs = np.random.randint(0, len(x), nrows*ncols)
+
     fig, axes = plt.subplots(nrows=6, ncols=6, figsize=(10, 10))
-    for ax, image, label in zip(axes.ravel(), x, y):
+    for i, ax in enumerate(axes.ravel()):
+        idx = shuffled_idxs[i]
+        image = x[idx]
+        label = y[idx]
+        
         ax.set_axis_off()
         ax.imshow(image, cmap=plt.cm.gray, interpolation="nearest")
         ax.set_title("Label: %i" % label)
@@ -90,6 +99,8 @@ def plot_latent_space(latent_output, y_train, n_classes):
         mask = y_train == i
         ax.scatter(*latent_output[mask].T, s=0.5, label=i)
 
+    ax.set(xlabel='Activation of node 1', ylabel='Activation of node 2')
+    
     lgnd = ax.legend()
     for handle in lgnd.legendHandles:
         handle.set_sizes([100])
